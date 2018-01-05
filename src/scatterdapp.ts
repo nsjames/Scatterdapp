1053d84f55d2bf71738a42e9a99e79934d6984e2
Shai James <skyandbeyond@gmail.com>
2018-01-05 06:13:26 +0200

    account and reclaim

--- src/scatterdapp.ts
+++ src/scatterdapp.ts
@@ -1,4 +1,4 @@
-import {EncryptedStream, Network, NetworkMessage, NetworkMessageTypes, ScatterError, RandomIdGenerator, ContractPermission, EOSService} from "scattermodels";
+import {EncryptedStream, Network, NetworkMessage, NetworkMessageTypes, ScatterError, RandomIdGenerator, ContractPermission} from "scattermodels";
 
 
 
