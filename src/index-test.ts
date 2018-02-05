901edb75b537e9830607bc5eea71aa094abc0510
Shai James <skyandbeyond@gmail.com>
2018-02-05 04:38:06 +0200

    scatter 2.0

--- src/index-test.ts
+++ src/index-test.ts
@@ -18,63 +18,38 @@ class IndexTest {
 		// In order to set up a signature provided instance of eosjs we need
 		// to predefine a network, and possibly options.
 		const network = { host:"192.168.56.101", port:8888 };
-		// const network = { host:"mainnet.eos.io", port:8080 };
-		const eosOptions = { expireInSeconds: 60, };
+		const eosOptions = {};
 
 		// Now this.scatter can be used to return a valid instance of eosjs that can be
 		// used with the Scatter extension. Trying to use a manually created eosjs instance
 		// with a fake scatter provider will fail due to WeakMap verification.
 		const eos = this.scatter.eos( Eos.Localnet, network, eosOptions );
 
-		console.log(this.scatter);
+		let identity = null;
 
-
-
-		document.getElementById('buy').addEventListener('click', () => {
-			eos.transfer('feasdf1', 'inita', 10, '').then(transaction => {
-				bindTrxData('buy_vals', transaction);
-			}).catch(e => { bindError('buy_vals', e) })
-		})
-		//
-		// document.getElementById('buy2').addEventListener('click', () => {
-		// 	let trx = {
-		// 		messages: [{
-		// 			code: 'eos',
-		// 			type: 'transfer',
-		// 			authorization: [], // Left out, will be calculated after
-		// 			data:{
-		// 				from:'[scatter]', // Left out, will be calculated after
-		// 				to:'inita',
-		// 				amount:1,
-		// 				memo:''
-		// 			}
-		// 		}],
-		// 		scope:['inita'], // Leave out the other
-		// 		signatures:[]
-		// 	};
-		//
-		// 	this.scatter.signWithAnyAccount(trx).then(transaction => {
-		// 		bindTrxData('buy2_vals', transaction);
-		// 	}).catch(e => { bindError('buy2_vals', e) })
-		// })
-		//
 		document.getElementById('ident').addEventListener('click', () => {
-			this.scatter.getIdentity(['email']).then(id => {
+			this.scatter.getIdentity(['account']).then(id => {
+				if(!id) return false;
+				this.scatter.useIdentity(id.hash);
+				identity = id;
 				console.log('Possible identity', id)
 			});
+		});
+
+		document.getElementById('buy').addEventListener('click', () => {
+			eos.transfer(identity.account.name, 'inita', 10, '').then(transaction => {
+				console.log('transaction', transaction);
+				bindTrxData('buy_vals', transaction);
+			}).catch(e => { bindError('buy_vals', e) })
+		});
+
+		document.getElementById('topup').addEventListener('click', () => {
+			let webEos = Eos.Localnet({httpEndpoint:`http://${network.host}:${network.port}`, keyProvider:'5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3'});
+			webEos.transfer('inita', identity.account.name, 100000, '').then(transaction => {
+				bindTrxData('topup_vals', transaction);
+			}).catch(e => { bindError('topup_vals', e) })
 		})
-		//
-		// document.getElementById('topup').addEventListener('click', () => {
-		// 	this.scatter.requestIdentity().then(account => {
-		// 		let webEos = (<any>window).Eos.Localnet({httpEndpoint:network.toEndpoint(), keyProvider:'5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3'});
-		// 		webEos.transfer('inita', account.name, 100000, '').then(transaction => {
-		// 			bindTrxData('topup_vals', transaction);
-		// 		}).catch(e => { bindError('topup_vals', e) })
-		// 	}).catch(e => { bindError('topup_vals', e) })
-		// })
-		//
-		//
-		//
+
 		function bindTrxData(elemId, transaction){
 			document.getElementById(elemId).innerHTML = `Transaction ID: <b>${transaction.transaction_id}</b> <br>
 													     Transaction Data: <code>${JSON.stringify(transaction.transaction.messages[0].data)}</code>`
